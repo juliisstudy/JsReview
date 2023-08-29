76672879fd18b1d1a0c0c5a871b9675dc13b5e03
Juli <31259481+juliisstudy@users.noreply.github.com>
2023-08-29 16:48:19 -0400

    review getElementbyId and button function

---
+++ JsReview.js
@@ -0,0 +1,15 @@
+document.getElementById("demo").innerHTML = "HTML content";
+
+function onClickButton (){
+    document.getElementById("demo").innerHTML = "Hello";
+}
+
+function doSomeMath(){
+    const PI = 3.14;
+    let x = 5;
+    let y = 6;
+    let z = x + y + PI;
+    document.getElementById("result").innerHTML = z;
+
+}
+            
