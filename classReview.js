b16c7e870ebf0524e4a10b88d9a7d6ab2274aa52
Juli <31259481+juliisstudy@users.noreply.github.com>
2023-09-04 17:39:41 -0400

    js spread operator

--- classReview.js
+++ classReview.js
@@ -26,3 +26,36 @@ hello = () => {
 hello = () => "hello world";
 hello = (val) => "hello" + val;
 hello = val => "hello" + val;
+
+//array
+const myArray = ['apple','banana','orange'];
+const myList = myArray.map((item)=><p>{item}</p>);
+
+//destructuring 
+function calculate(a,b){
+    const add = a + b;
+    const subtract = a - b;
+    const multiply = a * b;
+    const divide = a / b;
+    return [add, subtract,multiply,divide];
+}
+const [add,subtract,multiply,divide] = calculate(3,4);
+
+//destructuring objects
+const vehicleOne = {
+    brand:'Ford',
+    model:'M',
+    type:'car'
+}
+myVehicle(vehicleOne);
+function myVehicle({type,model,brand}){
+    const message = 'My' +type +'is a '+model+',and brand is'+brand;
+}
+
+// Spread Operator ... allows us to quickly copy all or part of existing array 
+//to a new array.
+const numbersOne = [1,2,3];
+const numbersTwo = [4,5,6];
+const numbersCombined = [...numbersOne, ...numbersTwo];
+
+
